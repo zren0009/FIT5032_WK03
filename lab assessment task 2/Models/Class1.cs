4d88f601b7856c779e1e4b7c6d9c4d074789f52c
Zichang Ren <zren0009@student.monash.edu>
2020-10-04 22:58:18 +0800

    123

---
+++ lab assessment task 2/Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace FIT5032_Week6.Models
+{
+    public class SampleFormViewModels
+    {
+    }
+    public class FormOneViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
